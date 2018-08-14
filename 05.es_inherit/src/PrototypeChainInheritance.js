22a7f3bcd5d065f30e30bbfc7a290d26dd0b7b16
webj2ee <1696627335@qq.com>
2018-08-14 17:12:31 +0800

    Signed-off-by: webj2ee <1696627335@qq.com>

--- 05.es_inherit/src/PrototypeChainInheritance.js
+++ 05.es_inherit/src/PrototypeChainInheritance.js
@@ -1,1 +1,26 @@
-console.log(1);
+// 动物类
+function Animal(name = "Animal") {
+
+    // 实例属性
+    this.name = name;
+
+    // 实例方法
+    this.sleep = function () {
+        console.log(this.name + '正在睡觉！');
+    }
+}
+
+// 原型方法
+Animal.prototype.eat = function (food) {
+    console.log(this.name + '正在吃：' + food);
+};
+
+////////////////////////////////////////////////////////////
+
+// 猫类
+function Cat() {
+    this.name = "cat";
+}
+
+Cat.prototype = new Animal();
+Cat.prototype.constructor = Cat;
