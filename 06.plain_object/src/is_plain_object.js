90359ff278f24e1aa6588acc4d69ec1d82a609c3
webj2ee <1696627335@qq.com>
2018-08-20 13:44:04 +0800

    Signed-off-by: webj2ee <1696627335@qq.com>

---
+++ 06.plain_object/src/is_plain_object.js
@@ -0,0 +1,22 @@
+import isPlainObject from "is-plain-object"
+
+isPlainObject(Object.create({}));
+//=> true
+isPlainObject(Object.create(Object.prototype));
+//=> true
+isPlainObject({foo: 'bar'});
+//=> true
+isPlainObject({});
+
+isPlainObject(1);
+//=> false
+isPlainObject(['foo', 'bar']);
+//=> false
+isPlainObject([]);
+//=> false
+isPlainObject(new Foo);
+//=> false
+isPlainObject(null);
+//=> false
+isPlainObject(Object.create(null));
+//=> false
