5570c0a093c346f7b053f3f114aee1dbefc1d890
WEBJ2EE <1696627335@qq.com>
2018-09-29 19:58:06 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

---
+++ 10.type_conversion/src/typeof.js
@@ -0,0 +1,9 @@
+console.log(typeof undefined)
+console.log(typeof true);
+console.log(typeof 42);
+console.log(typeof "42");
+console.log(typeof {life: 42});
+console.log(typeof Symbol());
+
+console.log(typeof null);
+console.log(typeof function a() {/* .. */});
