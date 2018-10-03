7dd930ddc488e6fc1d689ea8ab23776e58cc1b2b
WEBJ2EE <1696627335@qq.com>
2018-10-03 19:58:08 +0800

    Signed-off-by: WEBJ2EE <1696627335@qq.com>

---
+++ 11.this/src/hoisting1.js
@@ -0,0 +1,6 @@
+var a = 1;
+function foo(){
+    console.log(a);
+    var a = 2;
+}
+foo();
---
+++ 11.this/src/hoisting2.js
@@ -0,0 +1,9 @@
+var foo = function () {
+    console.log(2);
+};
+
+function foo(){
+    console.log(1);
+}
+
+foo();
